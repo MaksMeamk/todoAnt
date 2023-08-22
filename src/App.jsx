6dce4f60cbd5dd6b4878fe553d3a8420d3a51937
Макс Мацкевич <118908699+MaksMeamk@users.noreply.github.com>
2023-08-22 09:22:45 +0000

    ten minuts and finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,30 @@ import "./App.css";
 import CustomInput from "./Components/CustomInput";
 import ToDo from "./Components/ToDo";
 import { useState } from "react";
-import { List, Button, Row, Col } from "antd";
-
+import { List, Row, Col } from "antd";
 
 const App = () => {
   const [todos, setTodos] = useState([{ id: 1, title: "Сыр", status: false }]);
 
   const addTodo = (text) => {
-    const updatedTasks = [...todos, { id: todos.length + 1, title: text, status: false }];
+    const updatedTasks = [
+      ...todos,
+      { id: todos.length + 1, title: text, status: false },
+    ];
     setTodos(updatedTasks);
+    console.log(updatedTasks);
   };
   const handleDelete = (id) => {
-    console.log(id);
-    console.log(todos);
     const newTodos = todos.filter((item) => item.id !== id);
-    setTodos(newTodos)
-  }
+    setTodos(newTodos);
+    console.log(newTodos);
+  };
   const handleEdit = (id, text) => {
-    const newTodos = todos.map(item => item.id == id ? { ...item, title: text } : item);
-    setTodos(newTodos)
-  }
+    const newTodos = todos.map((item) =>
+      item.id == id ? { ...item, title: text } : item
+    );
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -33,7 +37,7 @@ const App = () => {
       <CustomInput addTodo={addTodo} />
       <Row justify="center">
         <Col>
-          <h1>              </h1>
+          <h1> </h1>
         </Col>
       </Row>
 
@@ -43,12 +47,17 @@ const App = () => {
             size="middle"
             bordered
             dataSource={todos}
-            renderItem={(item) => <ToDo item={item} handleDelete={handleDelete} handleEdit={handleEdit} />
-            }
+            renderItem={(item) => (
+              <ToDo
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+              />
+            )}
           />
         </Col>
       </Row>
-
     </div>
   );
 };
